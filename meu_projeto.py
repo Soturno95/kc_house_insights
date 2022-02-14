16747783693ccb6f0bb1c5f0c0df9876ece0969a
Soturno95 <edu.yori@gmail.com>
2022-02-14 15:56:03 -0300

    inicial commit

--- meu_projeto.py
+++ meu_projeto.py
@@ -303,3 +303,6 @@ st.dataframe(dfs30.style.format(subset=[ 'price','sell_price30%'], formatter="{:
 # c2.header('30% de lucro')
 # c2.dataframe(dfs30,height=400,width=400)
 #================================================================================================================
+
+
+# teste para o git
